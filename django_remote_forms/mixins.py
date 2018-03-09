cc8f575a7241c676db1cf05d43d3a3e5b1e16311
Steffan Ennis <steffanennis87@gmail.com>
2018-03-09 16:13:21 +1030

    Addressing #80 providing an extension mechanism

---
+++ django_remote_forms/mixins.py
@@ -0,0 +1,13 @@
+class ExtraAttributesMixin:
+    """Adds extra attributes to a forms fields
+
+    Value in the ``extra_attributes`` property will be added to the corresponding Key in fields dictionary
+    property
+    """
+    extras_attributes = {}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for key, value in self.extras_attributes.items():
+            field = self.fields.get(key)
+            field.extras = value
